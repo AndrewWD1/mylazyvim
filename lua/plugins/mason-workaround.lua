90c37e8f70513d8ae9628d917a4d06698d07eddf
Andrew Doumont <andrew.doumont@gmail.com>
2025-05-22 20:17:12 -0600

    fix for error described here: https://github.com/LazyVim/LazyVim/issues/6039

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,7 @@
+-- This is supposed to fix this bug: https://github.com/LazyVim/LazyVim/issues/6039
+-- Doesnt seem to work though
+
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
